d8c36a0d2aa375f887b3f71c00b43c6392bd769b
Fahime Zivdar <F.zivdar@nizek.com>
2022-05-07 15:22:34 +0430

    DataModules added
    ** dependency injection apply for any class need.

--- app/src/main/java/com/fahimezv/githubsearch/App.kt
+++ app/src/main/java/com/fahimezv/githubsearch/App.kt
@@ -1,11 +1,21 @@
 package com.fahimezv.githubsearch
 
 import android.app.Application
+import com.fahimezv.githubsearch.data.dataModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext
+import org.koin.core.logger.Level
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-
+        // Setup Koin
+        GlobalContext.startKoin {
+            androidLogger(Level.ERROR) // Koin Android Logger
+            androidContext(this@App) // Inject Android context
+            modules(dataModule) // Modules
+        }
     }
 
 }
---
+++ app/src/main/java/com/fahimezv/githubsearch/data/dataModule.kt
@@ -0,0 +1,66 @@
+package com.fahimezv.githubsearch.data
+
+import com.fahimezv.githubsearch.BuildConfig
+import com.fahimezv.githubsearch.data.network.generator.*
+import com.fahimezv.githubsearch.data.network.service.ServiceProvider
+import com.fahimezv.githubsearch.data.network.service.ServiceProviderImpl
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import retrofit2.Converter
+import retrofit2.converter.gson.GsonConverterFactory
+
+val dataModule = module {
+
+
+    //***************************************
+    //               Network                *
+    //***************************************
+
+    // ------------- Generators -------------
+    single {
+        //You can crete custom GsonConverterFactory here
+        val converterFactory: Converter.Factory = GsonConverterFactory.create()
+        converterFactory
+    }
+    factory {
+        if (BuildConfig.DEBUG) {    // development build
+            HttpLoggingInterceptor.Level.BODY
+        } else {    // production build
+            HttpLoggingInterceptor.Level.BASIC
+        }
+    }
+
+    single {
+        val networkEnvironment: NetworkEnvironment = NetworkEnvironmentImpl()
+        networkEnvironment
+    }
+
+    factory {
+        val networkHeadersGenerator: NetworkHeaderGenerator = NetworkHeaderGeneratorImpl()
+        networkHeadersGenerator
+    }
+
+
+    single {
+        val networkServiceFactory: NetworkServiceFactory = NetworkServiceFactoryImpl(
+            converterFactory = get(),
+            logLevel = get(),
+            environment = get(),
+            networkHeadersGenerator = get()
+        )
+        networkServiceFactory
+    }
+
+    factory {
+        val serviceProvider: ServiceProvider = ServiceProviderImpl(
+            serviceFactory = get()
+        )
+        serviceProvider
+    }
+
+    factory {
+        NetworkEnvironmentImpl()
+    }
+
+}
+
